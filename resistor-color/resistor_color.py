51779c6385e0216118211d8894bb30041ec6486d
massuhcodes <massuhcodes@gmail.com>
2022-06-17 15:32:53 -0400

    Adding completed resistor-color exercise

---
+++ resistor-color/resistor_color.py
@@ -0,0 +1,30 @@
+# Determine the resistor value based on the given color
+
+colorCodes = {
+    'black': 0,
+    'brown': 1,
+    'red': 2,
+    'orange': 3,
+    'yellow': 4,
+    'green': 5,
+    'blue': 6,
+    'violet': 7,
+    'grey': 8,
+    'white': 9
+}
+
+def color_code(color):
+    """
+    Return the code based on the given color
+    :param color: string - the color in question
+    :return: int - the code for the color
+    """
+    return colorCodes[color]
+
+
+def colors():
+    """
+    Returns a list of colors that would be found in a resistor
+    :return: [string] - all the colors
+    """
+    return list(colorCodes.keys())
