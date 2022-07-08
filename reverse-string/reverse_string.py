10a1ce0960965d5e81f7b084c6fd99d9f83c9612
massuhcodes <massuhcodes@gmail.com>
2022-07-07 23:01:54 -0400

    Adding completed reverse-string exercise

---
+++ reverse-string/reverse_string.py
@@ -0,0 +1,10 @@
+# reverse a string
+
+def reverse(text):
+    """
+    Return the reversed given text
+    
+    :param text: str - given string
+    :return: str - the reversed text
+    """
+    return text[::-1]
